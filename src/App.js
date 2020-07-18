9a7aca2a2e9c6450c889cc489162ba1d8e797f15
akademia108 <kursy@akademia108.pl>
2020-07-18 08:08:38 +0200

    new

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Countdown from 'react-countdown-now';
+
+import './App.css';
+
+class App extends Component {
+
+	
+	renderer = ({ days, hours, minutes, seconds}) => {
+		return <span>{days} dni {hours} godzin {minutes} minut {seconds} sekund</span>;
+	}
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+					
+					<h1>Zostało</h1>
+					<h2><Countdown date={'Mon, 8 Aug 2020 21:00:00'} renderer={this.renderer} /></h2>
+        </header>
+      </div>
+    );
+  }
+  
+  
+}
+
+export default App;
