a5fb17a36dceadffb77ece299bd2f27525e021cc
akademia108 <kursy@akademia108.pl>
2020-07-18 08:09:30 +0200

    Updates

---
+++ precache-manifest.ffcdd91de7fe94d094c8aa8e5d6e6e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad8f1f0d58551e90cc5fd97f33ea9e5",
+    "url": "/countdown/index.html"
+  },
+  {
+    "revision": "88b28b8b35b597baed47",
+    "url": "/countdown/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "cd1ee86c553a39ccb8b2",
+    "url": "/countdown/static/js/2.73a79d52.chunk.js"
+  },
+  {
+    "revision": "88b28b8b35b597baed47",
+    "url": "/countdown/static/js/main.3920b5df.chunk.js"
+  },
+  {
+    "revision": "6e598cc6c1442c396b59",
+    "url": "/countdown/static/js/runtime~main.6f2a1a9a.js"
+  }
+]);
